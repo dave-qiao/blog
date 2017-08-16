a78dd8de951e33e6ec4ca8501f595d8c59a6a18c
dave <18234172886@163.com>
2017-08-16 18:24:36 +0800

    Test listen port

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-// import Express from 'express';
-var express = require('express');
-// const app = express();
-var app = express();
+import Express from 'express';
+// var express = require('express');
+const app = Express();
+// var app = express();
 
 app.get('/', (req, res) => {
-  res.send('请求成功')
+  res.send('请求成功ssssss11111');
 });
 
 app.listen(3000, () => {
   console.log('app is litenting on port 3000');
-})
+});
+
 // function nodeStart() {
 //   console.log('Hello World');
 // }
